8853b551d6d2fa020ab8bd593d09d261d1823e37
Sriram <71959217+Sriram-bb63@users.noreply.github.com>
2022-11-19 19:25:36 +0530

    idk changes; works now

--- client.py
+++ client.py
@@ -1,50 +1,36 @@
 import socket
 import threading
-import sys
+from datetime import datetime
 
-
-def send_message():
-	while True:
-		message = input("")
-		if message == "ADMIN LOGIN":
-			username = input("Username: ")
-			password = input("Password: ")
-			message = f"ADMIN {username} {password}"
-		else:
-			message = f"{nickname}: {message}"
-		client.send(message.encode("utf-8"))
-
-def receive_message():
-	while True:
-		message = client.recv(1024).decode("utf-8")
-		global nickname
-		if message == "NICKNAME":
-			client.send(f"{nickname}".encode("utf-8"))
-		elif message == "ADMIN OK":
-			nickname = f"(A) {nickname}"
-			print("You are now an admin")
-		elif message == "ADMIN NOT OK":
-			print("Admin login failed")
-		else:
-			print(message)
-
-def run_threads():
-	print("run threads")
-	send_thread = threading.Thread(target=send_message)
-	send_thread.start()
-	receive_thread = threading.Thread(target=receive_message)
-	receive_thread.start()
-
-
+ENCODING_FORMAT = "utf-8"
 HOST = "127.0.0.1"
 PORT = 9090
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
-nope_nicknames = ["server", "admin"]
+def send_message():
+    while True:
+        message = input("")
+        time = datetime.now().strftime("%H:%M:%S")
+        message = f"{time}  " + f"{nickname}: " + message
+        client.send(message.encode(ENCODING_FORMAT))
+        # print(f"MESSAGE SENT to {client}")
+
+def receive_message():
+    while True:
+        message = client.recv(1024).decode(ENCODING_FORMAT)
+        if message == "NICKNAME":
+            client.send(f"{nickname}".encode(ENCODING_FORMAT))
+        else:
+            print(message)
+
+def run_threads():
+    send_thread = threading.Thread(target=send_message)
+    send_thread.start()
+    receive_thread = threading.Thread(target=receive_message)
+    receive_thread.start()
+
+
 nickname = input("Enter nickname: ")
-if nickname.lower() not in nope_nicknames:
-	run_threads()
-else:
-	print("CLIENT: Nickname not allowed")
-	sys.exit()
+run_threads()
--- server.py
+++ server.py
@@ -1,8 +1,7 @@
 import socket
 import threading
-import json
 
-
+ENCODING_FORMAT = "utf-8"
 HOST = "127.0.0.1"
 PORT = 9090
 
@@ -13,58 +12,43 @@ s.listen(5)
 clients = []
 nicknames = []
 
-with open("admins.json", "r") as f:
-    admins = json.load(f)
-active_admins = []
-
-def broadcast(message, sender_client):
+def broadcast(message, sender):
     for client in clients:
-        if client != sender_client:
-            client.send(message.encode("utf-8"))
-
+        if client != sender:
+            client.send(message.encode(ENCODING_FORMAT))
 
 def handle_clients(client, address):
     while True:
         try:
-            message = client.recv(1024).decode("utf-8")
-            if message.split()[0] == "ADMIN":
-                username = message.split()[1]
-                password = message.split()[2]
-                if username in admins and admins[username] == password:
-                    active_admins.append(client)
-                    i = clients.index(client)
-                    message = f"SERVER: {nicknames[i]} is now an admin"
-                    print(f"NEW ADMIN --- {address} | {nicknames[i]}")
-                    client.send("ADMIN OK".encode("utf-8"))
-                else:
-                    client.send("ADMIN NOT OK".encode("utf-8"))
+            message = client.recv(1024).decode(ENCODING_FORMAT)
+            print(message)
             broadcast(message, client)
         except Exception as e:
             i = clients.index(client)
-            # client.send("EXIT".encode("utf-8"))
             broadcast(f"SERVER: {nicknames[i]} disconnected", client)
-            print(f"DISCONNECT --- {address} | {nicknames[i]}")
+            print(f"DISCONNECT {nicknames[i]} | {address}")
             clients.pop(i)
             nicknames.pop(i)
+            print(f"ERROR\n{e}")
             break
-
 
 def receive_clients():
     while True:
         try:
             client, address = s.accept()
-            print(f"Connected with {address}")
-            client.send("NICKNAME".encode("utf-8"))
-            nickname = client.recv(1024).decode("utf-8")
+            print(f"NEW CONNECTION {address}")
+            client.send("NICKNAME".encode(ENCODING_FORMAT))
+            nickname = client.recv(1024).decode(ENCODING_FORMAT)
             clients.append(client)
             nicknames.append(nickname)
-            print(f"NAME CHANGE --- {address} -> {nickname}")
+            print(f"CONNECTION NAMED: {address} | {nickname}")
             broadcast(f"SERVER: {nickname} connected", client)
             t = threading.Thread(target=handle_clients, args=[client, address])
             t.start()
         except Exception as e:
             print(f"ERROR --- {e}")
             break
+
 
 print("SERVER START")
 

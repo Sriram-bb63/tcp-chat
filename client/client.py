6ccec85ab9fd9b688bb4d6d197ba1d85267d49bc
Sriram <71959217+Sriram-bb63@users.noreply.github.com>
2022-12-04 11:25:03 +0530

    file transfer works?

--- client/client.py
+++ client/client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 from datetime import datetime
+import sys
 
 ENCODING_FORMAT = "utf-8"
 HOST = "127.0.0.1"
@@ -12,16 +13,34 @@ client.connect((HOST, PORT))
 def send_message():
     while True:
         message = input("")
-        time = datetime.now().strftime("%H:%M:%S")
-        message = f"{time}  " + f"{nickname}: " + message
-        client.send(message.encode(ENCODING_FORMAT))
-        # print(f"MESSAGE SENT to {client}")
+        if message.startswith("SEND FILE"):
+            path = message.split()[-1]
+            filename = path.split("/")[-1]
+            with open(path, "r") as f:
+                content = f.read()
+                client.send(f"SEND FILE {filename} >>> {content}".encode(ENCODING_FORMAT))
+        else:
+            time = datetime.now().strftime("%H:%M:%S")
+            message = f"{time}  " + f"{nickname}: " + message
+            client.send(message.encode(ENCODING_FORMAT))
+            # print(f"MESSAGE SENT to {client}")
 
 def receive_message():
     while True:
         message = client.recv(1024).decode(ENCODING_FORMAT)
         if message == "NICKNAME":
             client.send(f"{nickname}".encode(ENCODING_FORMAT))
+        elif message.startswith("SEND FILE"):
+            filename = message.split()[2]
+            content = message.split(" >>> ")[-1]
+            # print(f"filename {filename}")
+            # print(f"content {content}")
+            path = f"client/files/{filename}"
+            with open(path, "w+") as f:
+                f.write(content)
+                print(f"{path} has been created")
+        elif message.endswith("END"):
+            sys.exit()
         else:
             print(message)
 
--- server/server.py
+++ server/server.py
@@ -9,6 +9,7 @@ HOST = "127.0.0.1"
 PORT = 9090
 ALT_PORT = 9091
 SERVER_TYPE = "Local Host"
+SERVER_MESSAGE_PREFIX = "SERVER------:"
 
 print("""
 Select server type:
@@ -44,25 +45,61 @@ except:
 s.listen(5)
 
 clients = {}
+admins = []
 with open("server/admins.json", "r") as f:
     admins_json = json.load(f)
 
 
+def admin_login(nickname, client, message):
+    print(f"ADMIN LOGIN ATTEMPT {nickname}")
+    password = message.split()[-1]
+    if nickname in admins_json and admins_json[nickname] == password:
+        admins.append(nickname)
+        client.send(f"{SERVER_MESSAGE_PREFIX} ADMIN LOGIN SUCCESS".encode(ENCODING_FORMAT))
+        print(f"ADMIN LOGIN SUCCESS {nickname}")
+        broadcast(f"{SERVER_MESSAGE_PREFIX} {nickname} is now an admin", client)
+    else:
+        client.send(f"{SERVER_MESSAGE_PREFIX} WRONG CREDENTIALS! ADMIN LOGIN FAILED".encode(ENCODING_FORMAT))
+        print(f"ADMIN LOGIN FAIL {nickname}")
+
+def kick_client(nickname, client, message):
+    if nickname in admins:
+        to_kick = message.split()[-1]
+        if to_kick not in clients:
+            client.send(f"{SERVER_MESSAGE_PREFIX} Cannot find {to_kick} in this server".encode(ENCODING_FORMAT))
+        else:
+            print(f"KICK OUT {to_kick} | {clients[to_kick].getpeername()} by {nickname}")
+            broadcast(f"{SERVER_MESSAGE_PREFIX} {to_kick} has been kicked out by {nickname}", client)
+            to_kick_client = clients[to_kick]
+            to_kick_client.send(f"{SERVER_MESSAGE_PREFIX} You have been kicked out by {nickname}".encode(ENCODING_FORMAT))
+            to_kick_client.send("END".encode(ENCODING_FORMAT))
+            del clients[to_kick]
+            print(clients)
+    else:
+        client.send(f"{SERVER_MESSAGE_PREFIX} You have to be an admin to use this command".encode(ENCODING_FORMAT))
+
+
 def broadcast(message, sender):
     for client in clients:
-        if client != sender:
+        if clients[client] != sender:
             clients[client].send(message.encode(ENCODING_FORMAT))
 
 def handle_clients(nickname, client, address):
     while True:
         try:
             message = client.recv(1024).decode(ENCODING_FORMAT)
-            print(message)
-            broadcast(message, client)
+            # print(message)
+            if "ADMIN LOGIN" in message:
+                admin_login(nickname, client, message)
+            elif "KICK" in message:
+                kick_client(nickname, client, message)
+            else:
+                broadcast(message, client)
         except Exception as e:
             del clients[nickname]
-            broadcast(f"SERVER------: {nickname} disconnected", client)
+            broadcast(f"{SERVER_MESSAGE_PREFIX} {nickname} disconnected", client)
             print(f"DISCONNECT {nickname} | {address}")
+            print(e)
             break
 
 def receive_clients():
@@ -74,7 +111,7 @@ def receive_clients():
             nickname = client.recv(1024).decode(ENCODING_FORMAT)
             clients[nickname] = client
             print(f"CONNECTION NAMED: {address} | {nickname}")
-            broadcast(f"SERVER------: {nickname} connected", client)
+            broadcast(f"{SERVER_MESSAGE_PREFIX} {nickname} connected", client)
             t = threading.Thread(target=handle_clients, args=[nickname, client, address])
             t.start()
         except Exception as e:
